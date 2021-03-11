6c095d09f76cfa956ec6c6380e81d1e9c6131a30
stm37 <79359895+stm37@users.noreply.github.com>
2021-03-11 15:28:10 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • tri [0895605030379]
+│ • gopay [0895605030379]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
